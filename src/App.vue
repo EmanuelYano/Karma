a2986ba0080c36e6307a921ed7ffb93e021c6705
Aluno <aluno@aluno.com>
2019-03-21 09:52:08 -0300

    1 commit

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,123 @@
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <template>
+        <div>
+          <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :nudge-width="200"
+            offset-x
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                color="indigo"
+                dark
+                v-on="on"
+              >
+                Menu as Popover
+              </v-btn>
+            </template>
+      
+            <v-card>
+              <v-list>
+                <v-list-tile avatar>
+                  <v-list-tile-avatar>
+                    <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+                  </v-list-tile-avatar>
+      
+                  <v-list-tile-content>
+                    <v-list-tile-title>John Leider</v-list-tile-title>
+                    <v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
+                  </v-list-tile-content>
+      
+                  <v-list-tile-action>
+                    <v-btn
+                      :class="fav ? 'red--text' : ''"
+                      icon
+                      @click="fav = !fav"
+                    >
+                      <v-icon>favorite</v-icon>
+                    </v-btn>
+                  </v-list-tile-action>
+                </v-list-tile>
+              </v-list>
+      
+              <v-divider></v-divider>
+      
+              <v-list>
+                <v-list-tile>
+                  <v-list-tile-action>
+                    <v-switch v-model="message" color="purple"></v-switch>
+                  </v-list-tile-action>
+                  <v-list-tile-title>Enable messages</v-list-tile-title>
+                </v-list-tile>
+      
+                <v-list-tile>
+                  <v-list-tile-action>
+                    <v-switch v-model="hints" color="purple"></v-switch>
+                  </v-list-tile-action>
+                  <v-list-tile-title>Enable hints</v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+      
+              <v-card-actions>
+                <v-spacer></v-spacer>
+      
+                <v-btn flat @click="menu = false">Cancel</v-btn>
+                <v-btn color="primary" flat @click="menu = false">Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-menu>
+        </div>
+      </template>
+
+      <template>
+        <v-form v-model="valid">
+          <v-container>
+            <v-layout>
+              <v-flex
+                xs12
+                md4
+              >
+                <v-text-field
+                  v-model="firstname"
+                  :rules="nameRules"
+                  :counter="10"
+                  label="First name"
+                  required
+                ></v-text-field>
+              </v-flex>
+      
+              <v-flex
+                xs12
+                md4
+              >
+                <v-text-field
+                  v-model="lastname"
+                  :rules="nameRules"
+                  :counter="10"
+                  label="Last name"
+                  required
+                ></v-text-field>
+              </v-flex>
+      
+              <v-flex
+                xs12
+                md4
+              >
+                <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  label="E-mail"
+                  required
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-form>
+      </template>
+      
     </v-content>
   </v-app>
 </template>
@@ -35,4 +151,33 @@ export default {
     }
   }
 }
+</script>
+<script>
+  export default {
+    data: () => ({
+      fav: true,
+      menu: false,
+      message: false,
+      hints: true
+    })
+  }
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      firstname: '',
+      lastname: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => v.length <= 10 || 'Name must be less than 10 characters'
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ]
+    })
+  }
 </script>
