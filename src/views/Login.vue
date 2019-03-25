fe404b3dc706f81130a2ccbb3b935964e85d6fdf
Aluno <aluno@aluno.com>
2019-03-25 17:28:04 -0300

    modificaçoes

---
+++ src/views/Login.vue
@@ -0,0 +1,9 @@
+<template>
+    <div >
+        <v-text-field label="Teste"></v-text-field>
+        <v-btn color="red">Teste</v-btn>
+        <!----<input v-model="email">
+        <input v-model="senha">
+        <v-btn color="blue">Entrar</v-btn> -->
+    </div>
+</template>
