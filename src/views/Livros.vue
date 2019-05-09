4a35fd9aef3990ed8d0f4f5445b5c44d200e1f02
Aluno <aluno@aluno.com>
2019-05-09 10:05:48 -0300

    att e adição

--- src/views/Livros.vue
+++ src/views/Livros.vue
@@ -1,10 +1,26 @@
 <template>
     <div>
+        <h1>
+            Hello {{x}}
+        </h1>
+        <br />
+        <v-btn @click="login"> livros</v-btn>
 
+        <input v-model="x" >
     </div>
 </template>
 <script>
-export default {
-    
-}
+    export default {
+      data(){
+          return{
+              x:"World!"
+          }
+      },
+      methods:{
+          login(){
+              if (this.x == "World!")
+              console.log(this.x)
+          }
+      }  
+    }
 </script>
